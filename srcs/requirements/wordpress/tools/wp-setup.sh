4737c20aabb18cc0cd0e93a0578c409a487544c7
abouguri <abouguri@42.fr>
2025-08-17 15:24:29 -0400

    rm mr cmnts

--- srcs/requirements/wordpress/tools/wp-setup.sh
+++ srcs/requirements/wordpress/tools/wp-setup.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 if [ ! -f /var/www/html/wp-config.php ]; then
-    # Download WordPress core files
     wp core download --allow-root
 
-    # Wait for MariaDB to be ready
     while ! mysqladmin ping -h mariadb --silent; do
         sleep 1
     done
 
-    # Now create wp-config.php
     wp config create --dbname=$MYSQL_DATABASE \
                     --dbuser=$MYSQL_USER \
                     --dbpass=$MYSQL_PASSWORD \
                     --dbhost=mariadb \
                     --allow-root
 
-    # Install WordPress
     wp core install --url=https://$DOMAIN_NAME \
                     --title=$WP_TITLE \
                     --admin_user=$WP_ADMIN_USER \
@@ -24,13 +20,11 @@ if [ ! -f /var/www/html/wp-config.php ]; then
                     --admin_email=$WP_ADMIN_EMAIL \
                     --allow-root
 
-    # Create a regular user
     wp user create $WP_USER $WP_USER_EMAIL \
                 --user_pass=$WP_USER_PASSWORD \
                 --role=author \
                 --allow-root
 
-    # Update permissions
     chown -R www-data:www-data /var/www/html
 fi
 
